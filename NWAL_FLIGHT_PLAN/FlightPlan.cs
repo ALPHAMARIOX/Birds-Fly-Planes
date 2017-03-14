dc4ed2d2656f4469c5392874cbf0ec553b2a2417
ALPHAMARIOX <alphamariox@gmail.com>
2017-03-14 11:54:06 -0600

    It's a start

---
+++ NWAL_FLIGHT_PLAN/FlightPlan.cs
@@ -0,0 +1,40 @@
+﻿/*
+Lee Harrold 
+ ° March 14, 2017
+ This program is to be used in documenting flight plans in an easier manner.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWAL_FLIGHT_PLAN
+{
+    class FlightPlan
+    {
+        //Attributes.
+        //Top form.
+        private DateTime fpDate; //Date flight was made.
+        private string registration;
+        private string aircraftType;
+        private string captain;
+        private string firstOfficer;
+        
+        //Middle Form.
+        private plr;
+        private apr;
+
+        //Bottom Form.
+
+        
+        //Public for the GUI and main program.
+        //Properties.
+        public DateTime FpDate { get; set; }
+        public string Registration { get; set; }
+        public string AircraftType { get; set; }
+        public string Captain { get; set; }
+        public string FirstOfficer { get; set; }
+    }
+}
